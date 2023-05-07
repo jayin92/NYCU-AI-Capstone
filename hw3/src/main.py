5ee28ceadff7d71f2003bd79ef129b94abb6b749
李杰穎 (Jay Lee) <jayin920805@gmail.com>
2023-05-07 18:35:18 +0800

    hw3: update program and report

--- hw3/src/main.py
+++ hw3/src/main.py
@@ -2,23 +2,37 @@ import random
 import math
 import os
 import argparse
+import time
+import numpy as np
 from tqdm import tqdm
 from itertools import combinations
 
 
 class Game:
-    def __init__(self, difficulty=0):
-        board_configurations = [
-            (9, 9, 10),
-            (16, 16, 25),
-            (16, 30, 99)
-        ]
-        self.h, self.w, self.num_of_mines = board_configurations[difficulty]
-        self.board = [[0 for _ in range(self.w)] for _ in range(self.h)]
-        self.shown_cell = [[False for _ in range(self.w)] for _ in range(self.h)]
-        self.mine_pos = set()
-        self.found_mines = set()
+    def __init__(self, difficulty=0, init_num_safe_cell=None):
+        '''
+        Initialize the game board
 
+        Parameters
+        ----------
+        difficulty : int
+            0: Easy, 1: Medium, 2: Hard
+
+        Returns
+        -------
+        None
+        '''
+        board_configurations = [
+            (9, 9, 10),   # Easy
+            (16, 16, 25), # Medium
+            (16, 30, 99)  # Hard
+        ]
+        self.h, self.w, self.num_of_mines = board_configurations[difficulty] # height, width, number of mines
+        self.board = [[0 for _ in range(self.w)] for _ in range(self.h)] # -1: mine, 0~8: number of mines around
+        self.shown_cell = [[False for _ in range(self.w)] for _ in range(self.h)] # Indicate the cell is opened or not
+        self.mine_pos = set() # The position of mines
+       
+        # Randomly generate mines
         while len(self.mine_pos) < self.num_of_mines:
             i = random.randrange(self.h)
             j = random.randrange(self.w)
@@ -26,7 +40,33 @@ class Game:
                 self.mine_pos.add((i, j))
                 self.board[i][j] = -1
 
+        self.init_num_safe_cell = init_num_safe_cell
+        num = round(math.sqrt(self.h * self.w)) if init_num_safe_cell is None else init_num_safe_cell
+        # num = 10
+        self.init_cells = set()
+        while len(self.init_cells) < num:
+            i = random.randrange(self.h)
+            j = random.randrange(self.w)
+            if (i, j) not in self.mine_pos and (i, j) not in self.init_cells:
+                self.init_cells.add((i, j))
+
+
     def open_cell(self, cell, safe):
+        '''
+        Open the cell and return the number of mines around the cell
+
+        Parameters
+        ----------
+        cell : tuple
+            The position of the cell
+        safe : bool
+            True if the cell is safe, False if the cell is a mine
+
+        Returns
+        -------
+        int
+            The number of mines around the cell, return -1 if wrongly opened
+        '''
         if ((cell in self.mine_pos) ^ (not safe)) or self.shown_cell[cell[0]][cell[1]]:
             return -1
         if cell not in self.mine_pos:
@@ -38,10 +78,22 @@ class Game:
 
         return self.board[cell[0]][cell[1]]
     
-    def cell_status(self, cell):
-        return self.board[cell[0]][cell[1]]
-    
     def get_hint(self, cell):
+        '''
+        Get the hint of the cell
+
+        Parameters
+        ----------
+        cell : tuple
+            The position of the cell
+
+        Returns
+        -------
+        list
+            The list of the cells around the cell
+        int
+            The number of mines around the cell
+        '''
         cnt = 0
         res = []
         for i in range(cell[0]-1, cell[0]+2):
@@ -57,6 +109,19 @@ class Game:
         return res, cnt
     
     def get_surround_mines(self, cell):
+        '''
+        Get the number of mines around the cell
+
+        Parameters
+        ----------
+        cell : tuple
+            The position of the cell
+
+        Returns
+        -------
+        int
+            The number of mines around the cell
+        '''
         cnt = 0
         for i in range(cell[0]-1, cell[0]+2):
             for j in range(cell[1]-1, cell[1]+2):
@@ -64,26 +129,36 @@ class Game:
                     cnt += 1
         return cnt
 
-    def is_mine(self, cell):
-        return cell in self.mine_pos
-    
-    def check_win(self):
-        return self.found_mines == self.mine_pos
     
     def get_init_safe_cells(self):
-        num = round(math.sqrt(self.h * self.w))
-        # num = 10
-        init_cells = set()
-        while len(init_cells) < num:
-            i = random.randrange(self.h)
-            j = random.randrange(self.w)
-            if (i, j) not in self.mine_pos and (i, j) not in init_cells:
-                init_cells.add((i, j))
+        '''
+        Get the initial safe cells
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        set
+            The set of the initial safe cells
+        '''
         
-        return init_cells
+        
+        return self.init_cells
 
     def print_board(self):
-        os.system('cls')
+        '''
+        Print the game board. ? means the cell is not opened yet. X means the cell is a mine. 0~8 means the number of mines around the cell.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         for i in range(self.h):
             for j in range(self.w):
                 if self.shown_cell[i][j]:
@@ -93,45 +168,130 @@ class Game:
             print()
 
 class Literal:
+    '''
+    A literal is a cell with a positive or negative sign. For example, (0, 0) is a positive literal, and (0, 0)' is a negative literal.
+    '''
     def __init__(self, cell, is_posi):
+        '''
+        Initialize the literal
+
+        Parameters
+        ----------
+        cell : tuple
+            The position of the cell
+        is_posi : bool
+            True if the literal is positive, False if the literal is negative
+
+        Returns
+        -------
+        None       
+        '''
         self.cell = cell
         self.posi = is_posi
 
     def __eq__(self, other):
+        '''
+        Check if two literals are the same
+        '''
         return self.cell == other.cell and self.posi == other.posi
     
     def __str__(self):
+        '''
+        Return the string of the literal
+        '''
         return str(self.cell) + ('' if self.posi else "'")
     
     def __hash__(self):
+        '''
+        Return the hash value of the literal
+        '''
         return hash(str(self))
     
 class Clause:
+    '''
+    A clause is a set of literals
+    '''
     def __init__(self, literals=[]):
+        '''
+        Initialize the clause
+
+        Parameters
+        ----------
+        literals : list
+            The list of literals
+        
+        Returns
+        -------
+        None
+        '''
         self.literals = set(literals)
         
     
     def __str__(self):
+        '''
+        Return the string of the clause
+        '''
         return "[" + ' '.join([str(l) for l in self.literals]) + "]"
     
     def __eq__(self, other):
+        '''
+        Check if two clauses are the same
+        '''
         return self.literals == other.literals
     
     def __len__(self):
+        '''
+        Return the number of literals in the clause
+        '''
         return len(self.literals)
     
     def __hash__(self):
+        '''
+        Return the hash value of the clause
+        '''
         return hash(str(self))
     
     def __copy__(self):
+        '''
+        Return the copy of the clause
+        '''
         return Clause(self.literals.copy())
     
     
 class KB:
+    '''
+    A knowledge base is a set of clauses
+    '''
     def __init__(self, clauses=set()):
+        '''
+        Initialize the knowledge base
+
+        Parameters
+        ----------
+        clauses : set
+            The set of clauses
+
+        Returns
+        -------
+        None
+        '''
         self.clauses = clauses
 
     def insert(self, clause: Clause, KB0):
+        '''
+        Insert a clause into the knowledge base
+
+        Parameters
+        ----------
+        clause : Clause
+            The clause to be inserted
+        KB0 : KB
+            The knowledge base that contains of claueses that are already inferred
+
+        Returns
+        -------
+        None
+        '''
         for clause1 in KB0.clauses:
             cell_pos = list(clause1.literals)[0].cell
             pos = list(clause1.literals)[0].posi
@@ -152,11 +312,28 @@ class KB:
             return None
         
         if len(clause.literals) >= 1:
-            self.clauses.add(clause)            
+            self.clauses.add(clause)
         # print(f"insert {clause}")
         # print(f"[\n{','.join([str(c) for c in self.clauses])}\n]")
 
 def matching_clauses(a: Clause, b: Clause):
+    '''
+    Check if two clauses can be matched using resolution
+
+    Parameters
+    ----------
+    a : Clause
+        The first clause
+    b : Clause
+        The second clause
+
+    Returns
+    -------
+    a : Clause
+        The first clause after matching
+    b : Clause
+        The second clause after matching
+    '''
     if len(a) > 2 and len(b) > 2:
         return a, b
     
@@ -168,6 +345,9 @@ def matching_clauses(a: Clause, b: Clause):
     
     if b.literals.issubset(a.literals):
         return b, None
+    
+    a = Clause(a.literals.copy())
+    b = Clause(b.literals.copy())
     
     comps = set()
     for i in a.literals:
@@ -189,7 +369,22 @@ def matching_clauses(a: Clause, b: Clause):
     
 
 class Player:
+    '''
+    The player class
+    '''
     def __init__(self, game: Game):
+        '''
+        Initialize the player
+
+        Parameters
+        ----------
+        game : Game
+            The game to be played
+
+        Returns
+        -------
+        None
+        '''
         self.game = game
         self.KB = KB(set())
         self.KB0 = KB(set())
@@ -199,17 +394,30 @@ class Player:
             self.safe.add(i)
             self.KB.insert(Clause([Literal(i, False)]), self.KB0)
 
-    def play(self):
+    def play(self, silent=False):
+        '''
+        Play the game
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         unmarked_cnt = 0
         while unmarked_cnt <= 10:
-            self.game.print_board()
-            print(f"# in KB: {len(self.KB.clauses)}, # in KB0: {len(self.KB0.clauses)}")
-            # for clause in self.KB.clauses:
-            #     print(clause, len(clause))
-            # print("----")
-            # for clause in self.KB0.clauses:
-            #     print(clause)
-            print(f"# single clause in KB: {len([clause for clause in self.KB.clauses if len(clause) == 1])}")
+            if not silent:
+                os.system('cls')
+                self.game.print_board()
+                print(f"# in KB: {len(self.KB.clauses)}, # in KB0: {len(self.KB0.clauses)}")
+                print(f"# single clause in KB: {len([clause for clause in self.KB.clauses if len(clause) == 1])}")
+                # for clause in self.KB.clauses:
+                #     print(clause, len(clause))
+                # print("----")
+                # for clause in self.KB0.clauses:
+                #     print(clause)
             updated = False
             if Clause([]) in self.KB.clauses:
                 self.KB.clauses.remove(Clause([]))
@@ -220,16 +428,17 @@ class Player:
                     lit = list(clause.literals)[0]
                     self.KB.clauses.remove(clause)
                     self.KB0.clauses.add(Clause(clause.literals.copy()))
-                    print(f"Open cell {lit.cell} with {lit.posi}")
+                    if not silent:
+                        print(f"Open cell {lit.cell} with {lit.posi}")
                     if lit.posi:
                         if self.game.open_cell(lit.cell, False) == -1:
                             print('Game Over!')
-                            exit(0)
+                            return -1
                         self.mine.add(lit.cell)
                     else:
                         if self.game.open_cell(lit.cell, True) == -1:
                             print('Game Over!')
-                            exit(0)
+                            return -1
                         self.safe.add(lit.cell)
                     # for clause1 in self.KB.clauses.copy():
                     #     print(clause1)
@@ -265,18 +474,23 @@ class Player:
                     break
             if updated:
                 continue
+            len_2_KB_clause = [x for x in list(self.KB.clauses.copy()) if len(x) == 2]
             KB_clause = list(self.KB.clauses.copy())
+            if silent:
+                print(f"{len(KB_clause)}")
             print("entering pairwise matching")
             unmarked_cnt += 1
-            for idx, i in tqdm(enumerate(KB_clause)):
-                for j in KB_clause[idx+1:]:
+            for i in tqdm(len_2_KB_clause):
+                for j in KB_clause:
+                    if i == j:
+                        continue
                     if i in self.KB.clauses:
                         self.KB.clauses.remove(i)
                     if j in self.KB.clauses:
                         self.KB.clauses.remove(j)
                     if(len(i) == 0 or len(j) == 0):
                         continue
-                    a, b = matching_clauses(Clause(i.literals.copy()), Clause(j.literals.copy()))
+                    a, b = matching_clauses(i, j)
                     
                     if a:
                         self.KB.insert(a, self.KB0)
@@ -298,11 +512,13 @@ class Player:
             if not updated:
                 if len(self.KB0.clauses) != self.game.h * self.game.w:
                     print("Stuck")
+                    return self.game.h * self.game.w - len(self.KB0.clauses)
                 else:
                     print("Win!")
-                exit(0)
+                    return 0
         print("Stuck")
-        exit(0)
+        return self.game.h * self.game.w - len(self.KB0.clauses)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -313,7 +529,48 @@ if __name__ == "__main__":
         help="difficulty level of the game 0: easy, 1: medium, 2: hard", 
         default=0
     )
+    parser.add_argument(
+        "-n", 
+        dest="n",
+        type=int, 
+        help="number of games to play",
+        default=1
+    )
+    # disable print board
+    parser.add_argument(
+        "-s",
+        "--silent",
+        dest="silent",
+        action="store_true",
+        help="disable print board",
+        default=False
+    )
+    parser.add_argument(
+        "-i",
+        "--init",
+        dest="init",
+        help="number of init safe cell",
+        type=int,
+        default=None
+    )
     args = parser.parse_args()
-    game = Game(args.difficulty)
-    player = Player(game)
-    player.play()
+    records = [] # win, time
+    random.seed(time.time())
+    for i in range(args.n):
+        game = Game(args.difficulty, args.init)
+        player = Player(game)
+        start = time.time()
+        res = player.play(args.silent)
+        end = time.time()
+        records.append((res, end-start))
+        print(f"Gmae {i+1} finished in {end-start} sec. with result {res}")
+    # Print status with windows
+    print("================= Status =================")
+    print(f"Play {args.n} games with difficulty {args.difficulty}")
+    print(f"Win: {len([i for i in records if i[0] == 0])}")
+    print(f"Lose: {len([i for i in records if i[0] == -1])}")
+    if len([i for i in records if i[0] > 0]) > 0:
+        print(f"Stuck: {len([i for i in records if i[0] > 0])}, avg stuck steps: {round(sum([i[0] for i in records if i[0] > 0])/len([i for i in records if i[0] > 0]), 2)}")
+    print(f"Average time: {round(sum([i[1] for i in records])/len(records), 2)} sec.")
+    print(f"Std time: {round(np.std([i[1] for i in records]), 2)} sec.")
+    print("==========================================")
